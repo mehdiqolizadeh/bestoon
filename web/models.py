dcb811ff7423df2f69abd874faaaaa1520d2d9cf
mehdiq <mehdiqolizadeh@gmail.com>
2019-03-29 17:18:34 +0430

    expence model is there

--- web/models.py
+++ web/models.py
@@ -1,3 +1,12 @@
-from django.db import models
+from __future__ import unicode_literals
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create models here
+
+class Expense(models.Model):
+    text = models.CharField(max_length=255)
+    date = models.DateTimeField()
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
