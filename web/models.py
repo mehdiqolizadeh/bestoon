442d031282c7415d8d8c4c5ef8ae74e18e66bb47
mehdiq <mehdiqolizadeh@gmail.com>
2019-03-30 14:34:49 +0430

    income in models and some beatification

--- web/models.py
+++ web/models.py
@@ -1,12 +1,23 @@
-from __future__ import unicode_literals
-
+#from __future__ import unicode_literals
+#import datetime
 from django.db import models
 from django.contrib.auth.models import User
 
 # Create models here
-
 class Expense(models.Model):
     text = models.CharField(max_length=255)
     date = models.DateTimeField()
     amount = models.BigIntegerField()
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    def __unicode__(self):
+        return (self.date , self.amount)
+
+
+
+class Income(models.Model):
+    text = models.CharField(max_length=255)
+    date = models.DateTimeField()
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    def __str__(self):
+        return (self.date , self.amount)
